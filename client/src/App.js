1dcf0d140ac96944ae0707462071bbde98367bbf
candrawahyuf19 <candrawahyuf@gmail.com>
2022-08-02 20:39:39 +0700

    perbaiki fe beranda, tentang

--- client/src/App.js
+++ client/src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
-import './App.css';
-// ---------------------Compponent Arsip Surat--------------------------------------// 
+import "./App.css";
+// ---------------------Compponent Arsip Surat--------------------------------------//
 import LoginAdminArsipSurat from "./components/arsipSurat/dashboardAdmin/LoginAdminArsipSurat";
 import DashboardAdminArsipSurat from "./components/arsipSurat/dashboardAdmin/DashboardAdminArsipSurat";
 import ProfilAdminArsipSurat from "./components/arsipSurat/dashboardAdmin/ProfilAdmin";
@@ -27,6 +27,7 @@ import DetailSuratKeluarPublic from "./components/arsipSurat/DetailSuratKeluar";
 
 // ---------------------Compponent Proposal--------------------------------------//
 import HomeProposal from "./components/proposal/Home";
+import TentangProposal from "./components/proposal/TentangProposal";
 import LoginAdminProposal from "./components/proposal/dashboardAdmin/LoginAdminProposal";
 import DashboardAdminProposal from "./components/proposal/dashboardAdmin/DashboardAdminProposal";
 import ProfilAdminProposal from "./components/proposal/dashboardAdmin/ProfilAdmin";
@@ -34,40 +35,114 @@ import EditProfilAdminProposal from "./components/proposal/dashboardAdmin/EditPr
 
 //Home
 import Home from "./components/Home";
+import Tentang from "./components/Tentang";
 
 function App() {
   return (
     <Routes>
       <Route exact path={"/"} element={<Home />} />
       <Route exact path={"/home"} element={<Home />} />
+      <Route exact path={"/tentang"} element={<Tentang />} />
 
-      <Route exact path="/login_admin_arsip_surat" element={<LoginAdminArsipSurat />} />
-      <Route exact path="/dashboard_admin_arsip_surat" element={<DashboardAdminArsipSurat />} />
-      <Route exact path="/profil_admin_arsip_surat" element={<ProfilAdminArsipSurat />} />
-      <Route exact path="/profil_admin_arsip_surat/edit" element={<EditProfilAdmin />} />
+      <Route
+        exact
+        path="/login_admin_arsip_surat"
+        element={<LoginAdminArsipSurat />}
+      />
+      <Route
+        exact
+        path="/dashboard_admin_arsip_surat"
+        element={<DashboardAdminArsipSurat />}
+      />
+      <Route
+        exact
+        path="/profil_admin_arsip_surat"
+        element={<ProfilAdminArsipSurat />}
+      />
+      <Route
+        exact
+        path="/profil_admin_arsip_surat/edit"
+        element={<EditProfilAdmin />}
+      />
 
       <Route exact path="/admin_surat_masuk" element={<SuratMasukPage />} />
-      <Route exact path="/admin_surat_masuk/tambah" element={<TambahSuratMasukPage />} />
-      <Route exact path="/admin_surat_masuk/detail/:id" element={<DetailSuratMasuk />} />
-      <Route exact path="/admin_surat_masuk/edit/:id" element={<EditSuratMasuk />} />
+      <Route
+        exact
+        path="/admin_surat_masuk/tambah"
+        element={<TambahSuratMasukPage />}
+      />
+      <Route
+        exact
+        path="/admin_surat_masuk/detail/:id"
+        element={<DetailSuratMasuk />}
+      />
+      <Route
+        exact
+        path="/admin_surat_masuk/edit/:id"
+        element={<EditSuratMasuk />}
+      />
       <Route exact path="/admin_surat_keluar" element={<SuratKeluarPage />} />
-      <Route exact path="/admin_surat_keluar/tambah" element={<TambahSuratKeluarPage />} />
-      <Route exact path="/admin_surat_keluar/detail/:id" element={<DetailSuratKeluar />} />
-      <Route exact path="/admin_surat_keluar/edit/:id" element={<EditSuratKeluar />} />
-      
+      <Route
+        exact
+        path="/admin_surat_keluar/tambah"
+        element={<TambahSuratKeluarPage />}
+      />
+      <Route
+        exact
+        path="/admin_surat_keluar/detail/:id"
+        element={<DetailSuratKeluar />}
+      />
+      <Route
+        exact
+        path="/admin_surat_keluar/edit/:id"
+        element={<EditSuratKeluar />}
+      />
+
       <Route exact path="/arsip_surat" element={<ArsipSuratPage />} />
-      <Route exact path="/arsip_surat/surat_masuk" element={<SuratMasukPublic />} />
-      <Route exact path="/arsip_surat/surat_masuk/detail/:id" element={<DetailSuratMasukPublic />} />
-      <Route exact path="/arsip_surat/surat_keluar" element={<SuratKeluarPublic />} />
-      <Route exact path="/arsip_surat/surat_keluar/detail/:id" element={<DetailSuratKeluarPublic />} />
+      <Route
+        exact
+        path="/arsip_surat/surat_masuk"
+        element={<SuratMasukPublic />}
+      />
+      <Route
+        exact
+        path="/arsip_surat/surat_masuk/detail/:id"
+        element={<DetailSuratMasukPublic />}
+      />
+      <Route
+        exact
+        path="/arsip_surat/surat_keluar"
+        element={<SuratKeluarPublic />}
+      />
+      <Route
+        exact
+        path="/arsip_surat/surat_keluar/detail/:id"
+        element={<DetailSuratKeluarPublic />}
+      />
 
       <Route exact path={"/pengajuan_proposal"} element={<HomeProposal />} />
+      <Route exact path={"/tentang_proposal"} element={<TentangProposal />} />
 
-      <Route exact path="/login_admin_proposal" element={<LoginAdminProposal />} />
-      <Route exact path="/dashboard_admin_proposal" element={<DashboardAdminProposal />} />
-      <Route exact path="/profil_admin_proposal" element={<ProfilAdminProposal />} />
-      <Route exact path="/profil_admin_proposal/edit" element={<EditProfilAdminProposal />} />
-
+      <Route
+        exact
+        path="/login_admin_proposal"
+        element={<LoginAdminProposal />}
+      />
+      <Route
+        exact
+        path="/dashboard_admin_proposal"
+        element={<DashboardAdminProposal />}
+      />
+      <Route
+        exact
+        path="/profil_admin_proposal"
+        element={<ProfilAdminProposal />}
+      />
+      <Route
+        exact
+        path="/profil_admin_proposal/edit"
+        element={<EditProfilAdminProposal />}
+      />
     </Routes>
   );
 }
--- client/src/components/arsipSurat/dashboardAdmin/LoginAdminArsipSurat.js
+++ client/src/components/arsipSurat/dashboardAdmin/LoginAdminArsipSurat.js
@@ -3,7 +3,7 @@ import { useNavigate } from "react-router-dom";
 import AuthService from "../../../services/authAdminArsipSurat.service";
 // Image
 import gambarHalamanLogin from "../../../asset/image/arsipSurat/loginArsipSurat.png";
-import { Eye, EyeSlash } from 'react-bootstrap-icons';
+import { Eye, EyeSlash } from "react-bootstrap-icons";
 // Css
 import "../../../css/arsipSurat/loginArsipSurat.css";
 
@@ -15,14 +15,13 @@ const LoginAdminArsipSurat = () => {
   const [message, setMessage] = useState("");
   const navigate = useNavigate();
 
-  const togglePassword =()=>{
-    if(passwordType==="password")
-    {
-     setPasswordType("text")
-     return;
+  const togglePassword = () => {
+    if (passwordType === "password") {
+      setPasswordType("text");
+      return;
     }
-    setPasswordType("password")
-  }
+    setPasswordType("password");
+  };
 
   const onChangeUsername = (e) => {
     const username = e.target.value;
@@ -38,13 +37,12 @@ const LoginAdminArsipSurat = () => {
     setLoading(true);
     AuthService.login(username, password).then(
       (response) => {
-        if(response.message === "Username atau Password Salah"){
+        if (response.message === "Username atau Password Salah") {
           setMessage(response.message);
-          setLoading(false);  
-        }
-        else if(response.message === "Log in Berhasil"){
           setLoading(false);
-          navigate("/dashboard_admin_arsip_surat"); 
+        } else if (response.message === "Log in Berhasil") {
+          setLoading(false);
+          navigate("/dashboard_admin_arsip_surat");
         }
       },
       (error) => {
@@ -60,60 +58,81 @@ const LoginAdminArsipSurat = () => {
     );
   };
   return (
-    <div className="container-fluid ps-md-0"> 
+    <div className="container-fluid ps-md-0">
       <div className="row g-0">
-        <div className="d-none d-md-flex col-md-4 col-lg-4 bg-image" style={{backgroundImage:`url(${gambarHalamanLogin})`}}></div>
+        <div
+          className="d-none d-md-flex col-md-4 col-lg-4 bg-image"
+          style={{ backgroundImage: `url(${gambarHalamanLogin})` }}
+        ></div>
         <div className="col-md-8 col-lg-8">
           <div className="login d-flex align-items-center py-5">
             <div className="container">
               <div className="row">
                 <div className="col-md-9 col-lg-8 mx-auto">
-                  <h3 className="login-heading mb-4 text-center">Login Admin</h3>  
-                  <form onSubmit={handleLogin}>
-                    <div className="form-group mb-3">
-                      <label htmlFor="username" className="label-login mb-1">Username</label>
-                      <input
-                        type="text"
-                        className="form-control"
-                        name="username"
-                        value={username}
-                        onChange={onChangeUsername}
-                      />
-                    </div>
-                    <label htmlFor="password" className="label-login mb-1">Password</label>
-                    <div className="input-group mb-3">
-                      <input
-                        type={passwordType}
-                        className="form-control"
-                        name="password"
-                        value={password}
-                        onChange={onChangePassword}
-                      />
-                      <div className="input-group-btn">
-                        <span className="btn default btn-eye" onClick={togglePassword}>
-                        { passwordType==="password"? <Eye className="eye-password" /> :<EyeSlash/> }
-                        </span>
+                  <div className="card card-login">
+                    <h3 className="login-heading mb-4 text-center">
+                      Login Admin
+                    </h3>
+                    <form onSubmit={handleLogin}>
+                      <div className="form-group mb-3">
+                        <label htmlFor="username" className="label-login mb-1">
+                          Username
+                        </label>
+                        <input
+                          type="text"
+                          className="form-control"
+                          name="username"
+                          value={username}
+                          onChange={onChangeUsername}
+                        />
                       </div>
-                    </div>
-
-                    {message && (
-                      <div className="form-group">
-                        <div className="alert alert-danger" role="alert">
-                          {message}
+                      <label htmlFor="password" className="label-login mb-1">
+                        Password
+                      </label>
+                      <div className="input-group mb-3">
+                        <input
+                          type={passwordType}
+                          className="form-control"
+                          name="password"
+                          value={password}
+                          onChange={onChangePassword}
+                        />
+                        <div className="input-group-btn">
+                          <span
+                            className="btn default btn-eye"
+                            onClick={togglePassword}
+                          >
+                            {passwordType === "password" ? (
+                              <Eye className="eye-password" />
+                            ) : (
+                              <EyeSlash />
+                            )}
+                          </span>
                         </div>
                       </div>
-                    )}
 
-                    <div className="d-grid">
-                      <button className="btn btn-lg btn-login text-uppercase fw-bold mb-2" type="submit" disabled={loading}>
-                        {loading && (
-                          <span className="spinner-border spinner-border-sm"></span>
-                        )}
-                        <span>Login</span>
-                      </button>
-                    </div>
+                      {message && (
+                        <div className="form-group">
+                          <div className="alert alert-danger" role="alert">
+                            {message}
+                          </div>
+                        </div>
+                      )}
 
-                  </form>
+                      <div className="d-grid">
+                        <button
+                          className="btn btn-lg btn-login text-uppercase fw-bold mb-2"
+                          type="submit"
+                          disabled={loading}
+                        >
+                          {loading && (
+                            <span className="spinner-border spinner-border-sm"></span>
+                          )}
+                          <span>Login</span>
+                        </button>
+                      </div>
+                    </form>
+                  </div>
                 </div>
               </div>
             </div>
@@ -121,7 +140,6 @@ const LoginAdminArsipSurat = () => {
         </div>
       </div>
     </div>
-    
   );
 };
 export default LoginAdminArsipSurat;
